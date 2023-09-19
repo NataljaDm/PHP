eb7e286b9881e005acca5884212fa07ee4c4b7f0
Natalja <tasia@inbox.lt>
2023-09-19 22:41:34 +0300

    route

--- invoices/routes/web.php
+++ invoices/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InvoiceController as I;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('invoices')->name('invoices-')->group(function () {
+    Route::get('/', [I::class, 'index'])->name('index'); //all invoices
+    Route::get('/show/{invoice}', [I::class, 'show'])->name('show'); //show one invoice
+
+    Route::get('/create', [I::class, 'create'])->name('create'); //show create form
+    Route::get('/edit/{invoice}', [I::class, 'edit'])->name('edit'); //show edit form
+    Route::get('/delete/{invoice}', [I::class, 'delete'])->name('delete'); //show delete confirmation
+
+    Route::post('/', [I::class, 'store'])->name('store'); //store new invoice
+    Route::put('/{invoice}', [I::class, 'update'])->name('update'); //update existing invoice
+    Route::delete('/{invoice}', [I::class, 'destroy'])->name('destroy'); //delete existing invoice
+
+});
+
+
 
 Auth::routes();
 
